8f46f07643413f0bb93ac817d7c1bccaeef58ca4
ShashankReddy0205 <asireddyshashankreddy@gmail.com>
2026-04-14 12:20:00 +0530

    Initial commit of src/pages/Researcher/index.js

---
+++ src/pages/Researcher/index.js
@@ -0,0 +1,5 @@
+export { default as ResearcherDashboard } from './ResearcherDashboard';
+export { default as MyProjects } from './MyProjects';
+export { default as DocumentUpload } from './DocumentUpload';
+export { default as Milestones } from './Milestones';
+export { default as Discussions } from './Discussions';
