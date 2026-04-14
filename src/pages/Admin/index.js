a111f5c4345a67faae94a2f42d161f4b85dd05c6
ShashankReddy0205 <asireddyshashankreddy@gmail.com>
2026-04-14 11:25:00 +0530

    Initial commit of src/pages/Admin/index.js

---
+++ src/pages/Admin/index.js
@@ -0,0 +1,5 @@
+export { default as AdminDashboard } from './AdminDashboard';
+export { default as ManageProjects } from './ManageProjects';
+export { default as ManageUsers } from './ManageUsers';
+export { default as ManageDocuments } from './ManageDocuments';
+export { default as ProjectProgress } from './ProjectProgress';
