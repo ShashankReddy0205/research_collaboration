e7642b2b6bd09311e562efac6c18bb942b591fdd
ShashankReddy0205 <asireddyshashankreddy@gmail.com>
2026-04-14 12:00:00 +0530

    Initial commit of src/pages/index.jsx

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+export { default as LandingPage } from './LandingPage';
+export { default as RoleSelectionPage } from './RoleSelectionPage';
+export { default as LoginPage } from './LoginPage';
+export { default as RegistrationPage } from './RegistrationPage';
+export { default as AboutPage } from './AboutPage';
+export { default as ContactPage } from './ContactPage';
+export { default as ProfilePage } from './ProfilePage';
+export { default as NotificationsPage } from './NotificationsPage';
+export { default as NotFoundPage } from './NotFoundPage';
+
